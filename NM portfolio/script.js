e3521d9f1d25e6c9e367fbc8655e5ab2ce35ea72
thaya1408 <132737199+thaya1408@users.noreply.github.com>
2023-11-15 14:10:51 +0530

    Add files via upload

---
+++ NM portfolio/script.js
@@ -0,0 +1,11 @@
+let typed = new typed ("multiple-text", {
+    strings: [
+        "Front-end Developer",
+         "Web-Developer",
+          "excel handler",
+        ],
+    typespeed: 50,
+    backspeed: 50,
+    backdelay: 1000,
+    loop: true,
+});
